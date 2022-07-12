9394cdf7c44306a0adc80bcce6bc9e1cc423538e
Andrew O <61236243+AndyOrv@users.noreply.github.com>
2022-07-12 16:10:52 +0100

    worked on door

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -8,9 +8,16 @@ public class DoorTrigger : MonoBehaviour
     [SerializeField]
     private GameObject door;
     public int upheight;
+    private Vector3 startPoint;
 
     bool isOpened = false;
 
+    private void Start()
+    {
+        startPoint = door.transform.position;
+    }
+
+    /*
     void OnTriggerEnter(Collider other)
     {
         if(!isOpened)
@@ -18,5 +25,21 @@ public class DoorTrigger : MonoBehaviour
             isOpened = true;
             door.transform.position = new Vector3(0,upheight,0);
         }
+
     }
+    */
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("door opened");
+
+        if (other.attachedRigidbody)
+        {
+            door.transform.position = new Vector3(0, upheight, 0);
+        }
+    }
+
+    //private void Update()
+    //{
+    //   door.transform.position = startPoint;
+    //}
 }
